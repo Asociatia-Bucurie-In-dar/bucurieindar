eb25615f92586d2e93b024a4e0c25847dc053020
fabianghe <fabianghenof@techie.com>
2024-02-06 10:32:17 +0200

    init commit

---
+++ components/Welcome/Welcome.tsx
@@ -0,0 +1,26 @@
+import { Title, Text, Anchor } from '@mantine/core';
+import classes from './Welcome.module.css';
+
+export function Welcome() {
+  return (
+    <>
+      <Title className={classes.title} ta="center" mt={100}>
+        Welcome to{' '} <br/>
+        <Text inherit variant="gradient" component="span" gradient={{ from: 'pink', to: 'yellow' }}>
+          Bucurie in Dar
+        </Text>
+      </Title>
+      <Text c="dimmed" ta="center" size="lg" maw={580} mx="auto" mt="xl">
+          This starter Next.js project includes a minimal setup for server side rendering, if you want
+        to learn more on Mantine + Next.js integration follow{' '}
+        <Anchor href="https://mantine.dev/guides/next/" size="lg">
+          this guide
+        </Anchor>
+        . To get started edit page.tsx file.
+      </Text>
+        <Text c="dimmed" ta="center" size="lg" maw={580} mx="auto" mt="xl">
+            Ä ö Ü ß, ă î ș ţ â, é à è û ç ë, 你好, ñ á é í ó ú ¿ ¡, русский, 日本語, 한국어, العربية, עברית, ελληνικά
+        </Text>
+    </>
+  );
+}
