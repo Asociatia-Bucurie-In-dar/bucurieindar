bf602a0956653484ab6c1ea9a7bf2f21b2c29fad
fabianghe <fabianghenof@techie.com>
2024-02-06 11:10:22 +0200

    change scalable to fix accessibility

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import '@mantine/core/styles.css';
 import React from 'react';
 import { MantineProvider, ColorSchemeScript } from '@mantine/core';
-import { theme } from '../theme';
+import { theme } from '@/theme';
 
 export const metadata = {
   title: 'Mantine Next.js template',
@@ -11,17 +11,14 @@ export const metadata = {
 export default function RootLayout({ children }: { children: any }) {
   return (
     <html lang="en">
-      <head>
-        <ColorSchemeScript />
-        <link rel="shortcut icon" href="/favicon.svg" />
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
-        />
-      </head>
-      <body>
-        <MantineProvider theme={theme}>{children}</MantineProvider>
-      </body>
+    <head>
+        <ColorSchemeScript/>
+        <link rel="shortcut icon" href="/favicon.svg"/>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width"/>
+    </head>
+    <body>
+    <MantineProvider theme={theme}>{children}</MantineProvider>
+    </body>
     </html>
   );
 }
