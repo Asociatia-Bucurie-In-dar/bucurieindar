499b0af5be875043274a3ecd86d502c71e1b7191
fabianghe <fabianghenof@techie.com>
2024-02-06 10:54:44 +0200

    add yarn

--- components/Welcome/Welcome.tsx
+++ components/Welcome/Welcome.tsx
@@ -5,7 +5,7 @@ export function Welcome() {
   return (
     <>
       <Title className={classes.title} ta="center" mt={100}>
-        Welcome to{' '} <br/>
+        Welcome to {' '} <br/>
         <Text inherit variant="gradient" component="span" gradient={{ from: 'pink', to: 'yellow' }}>
           Bucurie in Dar
         </Text>
