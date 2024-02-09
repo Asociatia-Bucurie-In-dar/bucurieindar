a37389af44b4451213a65bd1aecf9419e8cf4368
fabianghe <fabianghenof@techie.com>
2024-02-09 16:26:48 +0200

    fix?

--- components/Home/HomeAboutGeneral/HomeAboutGeneral.tsx
+++ components/Home/HomeAboutGeneral/HomeAboutGeneral.tsx
@@ -7,13 +7,17 @@ export function HomeAboutGeneral() {
     return <>
         <Container size="md" mt="xl" className={classes.wrapper}>
                     <div className={classes.body}>
-                        <Title className={classes.titleBig} align="center">
+                        <Center>
+                        <Title className={classes.titleBig} >
                             Potențialul copiilor
                         </Title>
+                        </Center>
                         <Divider mt="md" color="transparent"/>
-                        <Text fz="md" c="dimmed" align="center">
+                        <Center>
+                        <Text fz="md" c="dimmed" style={{textAlign: "center"}}>
                             Donația ta pentru cauza noastră creează un viitor mai strălucitor, oferind educație, sănătate și hrănire esențiale copiilor nevoiași. Fiecare contribuție aduce speranță și deschide uși către un univers de posibilități. Să construim împreună un viitor mai strălucitor. Donează acum și fă o diferență! Sprijinul tău este vital în misiunea noastră de a debloca potențialul unui copil. Cu fiecare donație, putem oferi mai mult decât nevoile de bază; oferim o bază pentru învățare și creștere pe tot parcursul vieții. Programele noastre se concentrează pe educație, îngrijire medicală și împuternicirea comunităților pentru a-și ridica cei mai tineri membri.
                         </Text>
+                        </Center>
                     </div>
                 <Center>
                     <Avatar className={classes.image}
