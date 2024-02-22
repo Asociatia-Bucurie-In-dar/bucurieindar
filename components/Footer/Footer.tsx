2e24696737d767762935b3f298a88d02c593c681
fabianghe <fabianghenof@techie.com>
2024-02-23 01:05:48 +0200

    footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,63 +1,20 @@
-import {Text, Container, ActionIcon, Group, rem, Title} from '@mantine/core';
+import {Text, Container, ActionIcon, Group, rem, Title, Center, SimpleGrid, Divider} from '@mantine/core';
 import { IconBrandTwitter, IconBrandYoutube, IconBrandInstagram } from '@tabler/icons-react';
 import classes from './Footer.module.css';
 import Link from "next/link";
 import {MyRoutePaths} from "@/utils/route-paths";
 
-const data = [
-    {
-        title: 'About',
-        links: [
-            { label: 'Features', link: MyRoutePaths.Home.link },
-            { label: 'Pricing', link: MyRoutePaths.Home.link },
-            { label: 'Support', link: MyRoutePaths.Home.link },
-            { label: 'Forums', link: MyRoutePaths.Home.link },
-        ],
-    },
-    {
-        title: 'Project',
-        links: [
-            { label: 'Contribute', link: MyRoutePaths.Home.link },
-            { label: 'Media assets', link: MyRoutePaths.Home.link },
-            { label: 'Changelog', link: MyRoutePaths.Home.link },
-            { label: 'Releases', link: MyRoutePaths.Home.link },
-        ],
-    },
-    {
-        title: 'Community',
-        links: [
-            { label: 'Join Discord', link: MyRoutePaths.Home.link },
-            { label: 'Follow on Twitter', link: MyRoutePaths.Home.link },
-            { label: 'Email newsletter', link: MyRoutePaths.Home.link },
-            { label: 'GitHub discussions', link: MyRoutePaths.Home.link },
-        ],
-    },
-];
-
-export function Footer() {
-    const groups = data.map((group) => {
-        const links = group.links.map((link, index) => (
-            <Text
-                key={index}
-                className={classes.link}
-                component={Link}
-                href={link.link}
-            >
+function link(link: { label: string, link: string }) {
+    return (
+        <Text component={Link} href={link.link} c="dimmed" size="sm">
                 {link.label}
-            </Text>
-        ));
-
-        return (
-            <div className={classes.wrapper} key={group.title}>
-                <Text className={classes.title}>{group.title}</Text>
-                {links}
-            </div>
-        );
-    });
-
+        </Text>
+    );
+}
+export function Footer() {
     return (
         <footer className={classes.footer}>
-            <Container className={classes.inner}>
+            <Container className={classes.inner} size="lg">
                 <div className={classes.logo}>
                     <Title className={classes.title} size={18} mb="xs">
                         <Text inherit variant="gradient" component="span" gradient={{ from: 'pink', to: 'yellow' }}>
@@ -67,10 +24,22 @@ export function Footer() {
                         </Text>
                     </Title>
                     <Text size="xs" c="dimmed" className={classes.description}>
-                        {"Aici o mini-descriere a asociației. Sau un motto. Sau ceva frumos. Bla bla bla."}
+                        {"Misiunea noastră este să aducem bucurie în inimile cât mai multor oameni, mici și mari, tineri și bătrâni, împreună cu voi."}
                     </Text>
                 </div>
-                <div className={classes.groups}>{groups}</div>
+            </Container>
+            <Divider color="transparent" mb="lg" />
+            <Container size="md">
+                <Center>
+            <SimpleGrid cols={6}>
+                {link(MyRoutePaths.Home)}
+                {link(MyRoutePaths.Projects)}
+                {link(MyRoutePaths.About)}
+                {link(MyRoutePaths.Gallery)}
+                {link(MyRoutePaths.Blog)}
+                {link(MyRoutePaths.Contact)}
+            </SimpleGrid>
+                </Center>
             </Container>
             <Container className={classes.afterFooter}>
                 <Text c="dimmed" size="sm">
@@ -78,15 +47,7 @@ export function Footer() {
                 </Text>
 
                 <Group gap={0} className={classes.social} justify="flex-end" wrap="nowrap">
-                    <ActionIcon size="lg" color="gray" variant="subtle">
-                        <IconBrandTwitter style={{ width: rem(18), height: rem(18) }} stroke={1.5} />
-                    </ActionIcon>
-                    <ActionIcon size="lg" color="gray" variant="subtle">
-                        <IconBrandYoutube style={{ width: rem(18), height: rem(18) }} stroke={1.5} />
-                    </ActionIcon>
-                    <ActionIcon size="lg" color="gray" variant="subtle">
-                        <IconBrandInstagram style={{ width: rem(18), height: rem(18) }} stroke={1.5} />
-                    </ActionIcon>
+                    
                 </Group>
             </Container>
         </footer>
