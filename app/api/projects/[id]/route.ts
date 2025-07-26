d539fffd55c70c132648b28c29cfd10ea73bee12
fabianghe <fabianghenof@techie.com>
2025-07-26 22:42:39 +0300

    re-add caching

--- app/api/projects/[id]/route.ts
+++ app/api/projects/[id]/route.ts
@@ -10,18 +10,24 @@ export async function GET(req: NextRequest) {
 
     // Try to get the cached total amount
     let totalAmount:number|null|undefined = null;
-    // if (cache.has(projectId)) 
-    // {
-    //     totalAmount = cache.get(projectId); 
-    // }
+    if (cache.has(projectId)) 
+    {
+         totalAmount = cache.get(projectId); 
+    }
 
     if (totalAmount === null || totalAmount === undefined) {
-        totalAmount = await prisma.donation.aggregate({
-            _sum: { amount: true },
-            where: { causeId: projectId }
-        }).then(r => {
-            return r._sum.amount ?? 0;
-        });
+        
+        try {
+            totalAmount = await prisma.donation.aggregate({
+                _sum: { amount: true },
+                where: { causeId: projectId }
+            }).then(r => {
+                return r._sum.amount ?? 0;
+            });   
+        }
+        catch (error) {
+            console.error('Error fetching total donation amount:', error);
+        }
 
         // Update the cache with the new value
         cache.set(projectId, totalAmount);
